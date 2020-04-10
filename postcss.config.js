3b049c671888b04fb5c347a4d864dc5a34cd77c2
Vuong B. Tru <trubavuong@gmail.com>
2020-04-10 15:32:16 +0700

    Add 'pagination.css'.

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,7 @@ module.exports = {
   plugins: [
     require('postcss-import'),
     require('postcss-nested'),
+    require('postcss-simple-vars'),
     require('autoprefixer'),
     require('cssnano')({
       preset: ['default', {
