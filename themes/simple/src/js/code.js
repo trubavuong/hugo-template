a73dd74ed8b98434156c9716f81131dc1f0faa03
Vuong B. Tru <trubavuong@gmail.com>
2020-04-12 23:34:59 +0700

    Add 'highlight.css'.

--- themes/simple/src/js/code.js
+++ themes/simple/src/js/code.js
@@ -1,24 +1,23 @@
-function addCopyButtonToHighlightPanel(panel) {
-  var panelTitle = panel.querySelector(".highlight-title");
-  if (panelTitle) {
-    var button = document.createElement("button");
-    button.className = "copy";
-    button.innerText = "Copy";
-    button.title = "Copy to clipboard";
-
-    addEvent(button, "click", function onclick() {
-      var code = panel.querySelector("code[data-lang]");
-      if (code) {
-        copy(code.innerText);
-        button.innerText = "Copied!";
-      }
-    });
-
-    panelTitle.appendChild(button);
-  }
-}
-
 (function () {
+  function addCopyButtonToHighlightPanel(panel) {
+    var panelTitle = panel.querySelector(".highlight-panel__title");
+    if (panelTitle) {
+      var button = document.createElement("button");
+      button.innerText = "Copy";
+      button.title = "Copy to clipboard";
+
+      addEvent(button, "click", function onclick() {
+        var code = panel.querySelector("code[data-lang]");
+        if (code) {
+          copy(code.innerText);
+          button.innerText = "Copied!";
+        }
+      });
+
+      panelTitle.appendChild(button);
+    }
+  }
+
   var panels = document.querySelectorAll('.highlight-panel');
   for (var i = 0; i < panels.length; i += 1) {
     addCopyButtonToHighlightPanel(panels[i]);
