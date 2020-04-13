e6a2a392cba07bbeb519ad33bb89149040056832
Vuong B. Tru <trubavuong@gmail.com>
2020-04-13 17:43:55 +0700

    Add 'theme.js'.

---
+++ themes/simple/src/js/theme.js
@@ -0,0 +1,35 @@
+if (window.localStorage) {
+  (function () {
+    var themeName = 'dark';
+    var themeKey = 'darkTheme';
+    var regex = /(^|\s)dark(\s|$)/;
+
+    function loadTheme() {
+      var className = document.body.className;
+      var hasThemeInClass = regex.test(className);
+      var theme = localStorage.getItem(themeKey);
+      if (theme > 0) {
+        if (!hasThemeInClass) {
+          document.body.className += (className && ' ') + themeName;
+        }
+      }
+      else if (hasThemeInClass) {
+        document.body.className = className.replace(regex, '');
+      }
+    }
+
+    function switchTheme() {
+      var theme = localStorage.getItem(themeKey);
+      localStorage.setItem(themeKey, theme > 0 ? '0' : '1');
+
+      loadTheme();
+    }
+
+    var elements = document.getElementsByClassName('theme-switch');
+    for (var i = 0; i < elements.length; i += 1) {
+      addEvent(elements[i], "click", switchTheme);
+    }
+
+    loadTheme();
+  })();
+}
