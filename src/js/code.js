9eeb3cd667aa490179e74c460b745d3bc8b1d29a
Vuong B. Tru <trubavuong@gmail.com>
2020-05-07 09:24:21 +0700

    Refactor js.

--- src/js/code.js
+++ src/js/code.js
@@ -1,21 +1,27 @@
 (function () {
+  function copyCodeblock(e) {
+    var button = e.target || e.srcElement;
+    if (button) {
+      var panel = button.parentNode.parentNode;
+      var code = panel.querySelector("code[data-lang]");
+      if (code) {
+        copy(code.innerText);
+
+        button.innerText = "Copied!";
+        setTimeout(function () {
+          button.innerText = "Copy";
+        }, 2000);
+      }
+    }
+  }
+
   function addCopyButtonToCodeblock(panel) {
     var panelHeader = panel.querySelector(".codeblock__header");
     if (panelHeader) {
       var button = document.createElement("button");
       button.innerText = "Copy";
 
-      addEvent(button, "click", function onclick() {
-        var code = panel.querySelector("code[data-lang]");
-        if (code) {
-          copy(code.innerText);
-
-          button.innerText = "Copied!";
-          setTimeout(function () {
-            button.innerText = "Copy";
-          }, 2000);
-        }
-      });
+      addEvent(button, "click", copyCodeblock);
 
       panelHeader.appendChild(button);
     }
--- src/js/share_text.js
+++ src/js/share_text.js
@@ -1,19 +1,17 @@
 (function () {
-  function addCopyLinkFunctionToButton(button) {
-    addEvent(button, "click", function onclick(e) {
-      e.preventDefault();
+  function copyLink(e) {
+    e.preventDefault();
 
-      copy(location.origin + location.pathname);
+    copy(location.origin + location.pathname);
 
-      button.innerText = "Copied!";
-      setTimeout(function () {
-        button.innerText = "Copy Link";
-      }, 2000);
-    });
+    button.innerText = "Copied!";
+    setTimeout(function () {
+      button.innerText = "Copy Link";
+    }, 2000);
   }
 
   var buttons = document.querySelectorAll(".copy-link");
   for (var i = 0; i < buttons.length; i += 1) {
-    addCopyLinkFunctionToButton(buttons[i]);
+    addEvent(buttons[i], "click", copyLink);
   }
 })();
